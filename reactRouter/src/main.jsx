8889033756497b3409842bb25975b78a0442decd
Akshat-Bindal <biakshat31@gmail.com>
2024-11-26 02:07:48 +0530

    push 6

---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,45 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import {About,Home,Contact,User,Github,githubInfoLoader} from './components/index.js'
+import '../src/index.css'
+
+// const router= createBrowserRouter(
+//   [{
+//     path:"/",
+//     element:<Layout/>,
+//     children:[
+//       {
+//         path:"",
+//         element:<Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       }
+//     ]
+
+//   }]
+// )
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+    </Route>
+  )
+)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
